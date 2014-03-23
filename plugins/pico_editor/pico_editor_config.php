0b8162a1eaf83ca517a9dd956eb0f3efe10e826b
Jayesh Gohel <jayesh.gohel@gmail.com>
2014-03-23 20:07:36 +0530

    Updating password

--- plugins/pico_editor/pico_editor_config.php
+++ plugins/pico_editor/pico_editor_config.php
@@ -6,6 +6,7 @@ global $pico_editor_password;
  * This should be a sha1 hash of your password.
  * Use a tool like http://www.sha1-online.com to generate.
  */
-$pico_editor_password = '665bbc24c80dba0da85fd5d135939dc1dc31df0d';
+// $pico_editor_password = '665bbc24c80dba0da85fd5d135939dc1dc31df0d';
+$pico_editor_password = "2fcf137106d8c85cbe33ea16e76bd2d5170e6dd9";
 
 ?>
