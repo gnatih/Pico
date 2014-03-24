a0d5f9b12a9e493b0c4cb08af7eebf64c5977d07
Jayesh Gohel <jayesh.gohel@gmail.com>
2014-03-24 14:10:20 +0530

    Updating styles, adding jquery

--- themes/powertech/js/script.js
+++ themes/powertech/js/script.js
@@ -2,5 +2,12 @@ $(function(){
   href = window.location.pathname;
 
   $("a[href='" + href + "']").parent("li").addClass("active");
-  console.log(href);
+
+
+  li = $(".sidebar li.is-current, .sidebar li.is-parent");
+
+
+// $(".sidebar li.is-parent").find("ul").slideDown();
+  li.find("ul").show();
+
 });
